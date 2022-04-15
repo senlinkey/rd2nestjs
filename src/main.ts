76a8208872f8bd91389a056719d65193020302d1
洪森林 <hongsenlin@newlink.com>
2022-04-16 01:30:40 +0800

    handing malicious request data

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { ValidationPipe } from "@nestjs/common";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //开启全局校验的 pipe, 配合 class-validator class-transformer
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, // 将可接受的属性列入白名单, 任何未包含在白名单中的属性自动从生成的对象中剥离
+    forbidNonWhitelisted: true, // 如果存在非白名单的属性, 立即抛错
+  }));
   await app.listen(3000);
 }
 
