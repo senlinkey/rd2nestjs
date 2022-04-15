8833d1d7b69276ef90449df0308158abd0fec154
洪森林 <hongsenlin@newlink.com>
2022-04-16 01:36:26 +0800

    feat: autotransform payloads to dto instance

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true, // 将可接受的属性列入白名单, 任何未包含在白名单中的属性自动从生成的对象中剥离
     forbidNonWhitelisted: true, // 如果存在非白名单的属性, 立即抛错
+    transform: true, // 将有效载荷转换为 dto 实例
   }));
   await app.listen(3000);
 }
