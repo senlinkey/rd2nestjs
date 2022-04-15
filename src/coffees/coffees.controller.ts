0c53f0c61f940cd5c9a00ceace1697bf022304a6
洪森林 <hongsenlin@newlink.com>
2022-04-16 00:10:51 +0800

    feat: respons code

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,10 +1,12 @@
-import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Res } from "@nestjs/common";
+import { Response } from "express";
 
 @Controller("coffees")
 export class CoffeesController {
 
   @Get()
-  findAll() {
+  findAll(@Res() response: Response) {
+    // return response.status(200).send(`This action returns all coffees`);
     return `This action returns all coffees`;
   }
 
@@ -14,6 +16,7 @@ export class CoffeesController {
   }
 
   @Post()
+  @HttpCode(HttpStatus.GONE)
   create(@Body() body) {
     return body;
   }
