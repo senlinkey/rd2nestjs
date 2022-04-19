63a74ddb72b505c85d959e0d91b4a5a4ee9d8568
洪森林 <>
2022-04-20 00:58:26 +0800

    feat: create a dynamic module

--- src/coffee-rating/coffee-rating.module.ts
+++ src/coffee-rating/coffee-rating.module.ts
@@ -1,10 +1,18 @@
 import { CoffeesModule } from "@/coffees/coffees.module";
 import { Module } from "@nestjs/common";
 import { CoffeeRatingService } from "./coffee-rating.service";
+import { DatabaseModule } from "@/database/database.module";
 
 @Module({
-  providers: [CoffeeRatingService],
-  imports: [CoffeesModule]
+  imports: [
+    DatabaseModule.register({
+      type: "postgres",
+      host: "localhost",
+      password: "123456",
+      port: 5432,
+    }),
+    CoffeesModule],
+  providers: [CoffeeRatingService]
 })
 export class CoffeeRatingModule {
 }
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,17 +1,29 @@
-import { Module } from "@nestjs/common";
-import { createConnection } from "typeorm";
+import { DynamicModule, Module } from "@nestjs/common";
+import { ConnectionOptions, createConnection } from "typeorm";
 
 @Module({
-  providers: [
-    {
-      provide: "CONNECTION",
-      useValue: createConnection({
-        type: "postgres",
-        host: "localhost",
-        port: 5432
-      })
-    }
-  ]
+  // providers: [
+  //   {
+  //     provide: "CONNECTION",
+  //     useValue: createConnection({
+  //       type: "postgres",
+  //       host: "localhost",
+  //       password: "123456",
+  //       port: 5432
+  //     })
+  //   }
+  // ] // 静态链接
 })
 export class DatabaseModule {
+  static register(options: ConnectionOptions): DynamicModule {
+    return {
+      module: DatabaseModule,
+      providers: [
+        {
+          provide: "CONNECTION",
+          useValue: createConnection(options)
+        }
+      ]
+    };
+  }
 }
