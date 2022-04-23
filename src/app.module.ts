e3bb449009bce91422a08f6d37ace0ee2d8671e1
洪森林 <>
2022-04-23 23:37:51 +0800

    53 Catch Exceptions with Filters

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module, ValidationPipe } from "@nestjs/common";
+import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { CoffeesModule } from "./coffees/coffees.module";
@@ -7,7 +7,6 @@ import { DatabaseModule } from "./database/database.module";
 import { ConfigModule } from "@nestjs/config";
 import { CoffeeRatingModule } from "@/coffee-rating/coffee-rating.module";
 import AppConfig from "@/config/app.config";
-import { APP_PIPE } from "@nestjs/core";
 
 @Module({
   imports: [
@@ -39,10 +38,13 @@ import { APP_PIPE } from "@nestjs/core";
     DatabaseModule
   ],
   controllers: [AppController],
-  providers: [AppService, {
-    provide: APP_PIPE,
-    useClass: ValidationPipe
-  }]
+  providers: [
+    AppService
+    // {
+    //   provide: APP_PIPE,
+    //   useClass: ValidationPipe
+    // }
+  ]
 })
 
 export class AppModule {
