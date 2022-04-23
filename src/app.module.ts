6e607ed10eb77530a72259c0d59d3df504dd25f5
洪森林 <>
2022-04-23 23:22:02 +0800

    understand bind technique

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from "@nestjs/common";
+import { Module, ValidationPipe } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { CoffeesModule } from "./coffees/coffees.module";
@@ -7,6 +7,7 @@ import { DatabaseModule } from "./database/database.module";
 import { ConfigModule } from "@nestjs/config";
 import { CoffeeRatingModule } from "@/coffee-rating/coffee-rating.module";
 import AppConfig from "@/config/app.config";
+import { APP_PIPE } from "@nestjs/core";
 
 @Module({
   imports: [
@@ -38,7 +39,10 @@ import AppConfig from "@/config/app.config";
     DatabaseModule
   ],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [AppService, {
+    provide: APP_PIPE,
+    useClass: ValidationPipe
+  }]
 })
 
 export class AppModule {
