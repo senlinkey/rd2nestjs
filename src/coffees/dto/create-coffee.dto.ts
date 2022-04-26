4e1791e6443890da8b3912485823614f29f721ec
洪森林 <>
2022-04-26 23:58:05 +0800

    63 Decorating Model Properties

--- src/coffees/dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto.ts
@@ -1,13 +1,16 @@
 import { IsString } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateCoffeeDto {
-
+  @ApiProperty({ description: "咖啡的名字" })
   @IsString()
   readonly name: string;
 
+  @ApiProperty({ description: "咖啡的牌子" })
   @IsString()
   readonly brand: string;
 
+  @ApiProperty({ example: ["猫屎"] })
   @IsString({ each: true })
   readonly flavors: string[];
 }
