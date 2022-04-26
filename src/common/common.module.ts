3d0baf93ddb2c1ffec16f7825e471c8c3ab41487
洪森林 <>
2022-04-26 23:08:22 +0800

    59 Bonus Add Request Logging with Middleware

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,13 +1,17 @@
-import { Module } from "@nestjs/common";
+import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
 import { APP_GUARD } from "@nestjs/core";
 import { ApiKeyGuard } from "@/common/guards/api-key.guard";
 import { ConfigModule } from "@nestjs/config";
+import { LoggingMiddleware } from "@/common/middleware/logging.middleware";
 
 @Module({
-  imports:[ConfigModule],
+  imports: [ConfigModule],
   providers: [
     { provide: APP_GUARD, useClass: ApiKeyGuard } // 全局使用时, 不能使用依赖注入, 改为这种
   ]
 })
-export class CommonModule {
+export class CommonModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
+    consumer.apply(LoggingMiddleware).forRoutes("*");
+  }
 }
